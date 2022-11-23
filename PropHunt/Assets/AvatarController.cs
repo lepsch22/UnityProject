3bea24476ce76332f783f3274a97fecf909ce28b
Ben Lepsch <72486394+lepsch22@users.noreply.github.com>
2022-11-23 15:40:58 -0600

    Hunter made

---
+++ PropHunt/Assets/AvatarController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MapTransforms
+{
+    public Transform vrTarget;
+    public Transform ikTarget;
+
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void VRMapping() 
+    {
+        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+
+    }
+
+
+}
+public class AvatarController : MonoBehaviour
+{
+    [SerializeField] private MapTransforms head;
+    [SerializeField] private MapTransforms leftHand;
+    [SerializeField] private MapTransforms rightHand;
+
+    [SerializeField] private float turnSmoothness;
+
+    [SerializeField] private Transform ikHead;
+    [SerializeField] private Vector3 headBodyOffset;
+
+    private void LateUpdate()
+    {
+        transform.position = ikHead.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward,Vector3.ProjectOnPlane(ikHead.forward, Vector3.up).normalized,Time.deltaTime*turnSmoothness);
+
+        head.VRMapping();
+        leftHand.VRMapping();
+        rightHand.VRMapping();
+    }
+}
