3b55aa95535c0efe4d99a572cbeffcddb9dc5278
connor <connor-sindt@uiowa.edu>
2022-12-07 16:12:56 -0600

    resize zuck and add hunter v prop spawning

--- PropHunt/Assets/NetworkPlayerSpawner.cs
+++ PropHunt/Assets/NetworkPlayerSpawner.cs
@@ -6,31 +6,26 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
 
-    public GameObject player;
+    public GameObject playerHunter;
     public GameObject playerProp;
 
-    GameObject gamedata;
+    private GameObject gamedata;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
 
-        //gamedata = GameObject.Find("GameData");
+        gamedata = GameObject.Find("GameData");
 
-        //if(gamedata.GetComponent<GameData>().prop){
-        //    Debug.Log("Spawn Prop");
+        if(gamedata.GetComponent<GameData>().prop){
+            Instantiate(playerProp, transform.position, transform.rotation);
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Prop", transform.position, transform.rotation);
 
-        /*
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Hunter", transform.position, transform.rotation);
-        Instantiate(player, transform.position, transform.rotation);
-        */
-        Instantiate(playerProp, transform.position, transform.rotation);
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Prop", transform.position, transform.rotation);
-
-        //} else {
-        //    Debug.Log("Spawn Hunter");
-        //    spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Hunter", transform.position, transform.rotation);
-        // }
+        } else {
+            //Debug.Log("Spawn Hunter");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Hunter", transform.position, transform.rotation);
+            Instantiate(playerHunter, transform.position, transform.rotation);
+         }
 
 
     }
