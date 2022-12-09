e6062554b3fd847da32678a80b1c453fb955cfd9
Ben Lepsch <72486394+lepsch22@users.noreply.github.com>
2022-12-09 14:29:46 -0600

    Fixed jumps, and colliders

--- PropHunt/Assets/jumpController.cs
+++ PropHunt/Assets/jumpController.cs
@@ -10,24 +10,39 @@ public class jumpController : MonoBehaviour
 
 
     public GameObject rb;
-
-    private bool _isGrounded => Physics.Raycast(
+    public bool _isGrounded = true;
+    /*
+    private bool _isGrounded2 => Physics.Raycast(
         new Vector2(rb.transform.position.x,rb.transform.position.y+1f),Vector3.down,5.0f);
+    */
 
     void Start()
     {
-        jumpActionReference.action.performed += OnJump;
+        jumpActionReference.action.performed += Jump;
         
     }
+    /*
     private void OnJump(InputAction.CallbackContext obj) {
         Debug.Log($"TEST");
         if (!_isGrounded) return;
         Debug.Log($"Adding Force");
         rb.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
     }
-    void Update()
+    */
+    private void Jump(InputAction.CallbackContext obj) 
     {
-        
+        Debug.Log("Attempt Jump");
+        if (_isGrounded) 
+        {
+            _isGrounded = false;
+            rb.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+        }
+
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isGrounded = true;
+    }
+
 }
 //Disable make the
