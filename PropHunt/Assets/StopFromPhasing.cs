29e9ea0e469466e33f46944ef6802ad851f3e730
Ben Lepsch <72486394+lepsch22@users.noreply.github.com>
2022-12-08 23:50:43 -0600

    Continous dynamic is the best for collision but still not great

---
+++ PropHunt/Assets/StopFromPhasing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopFromPhasing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform previousPosition;
+    private Transform currPosition;
+    void Start()
+    {
+        previousPosition = gameObject.transform;
+    }
+    private void Update()
+    {
+        previousPosition = gameObject.transform;
+    }
+
+    private void OnCollisionEnter(Collision co) {
+        gameObject.transform.position = previousPosition.position;
+
+
+        
+    }
+}
